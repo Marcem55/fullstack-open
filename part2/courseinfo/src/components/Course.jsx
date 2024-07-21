5877536ed2b3ba06a458b1d460d3cc54f56c109f
Marcem55 <malacalzamarcelo@gmail.com>
2024-07-21 20:34:24 -0300

    [ADD] exercises 2.1 to 2.5 completed

---
+++ part2/courseinfo/src/components/Course.jsx
@@ -0,0 +1,42 @@
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  );
+};
+
+const Header = (props) => {
+  return <h1>{props.course}</h1>;
+};
+
+const Content = (props) => {
+  return (
+    <div>
+      {props.parts.map((part) => (
+        <Part key={part.id} part={part.name} exercises={part.exercises} />
+      ))}
+    </div>
+  );
+};
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part} {props.exercises}
+    </p>
+  );
+};
+
+const Total = (props) => {
+  const total = props.parts.reduce((sum, part) => (sum += part.exercises), 0);
+  return (
+    <div>
+      <b>Total of {total} exercises</b>
+    </div>
+  );
+};
+
+export default Course;
