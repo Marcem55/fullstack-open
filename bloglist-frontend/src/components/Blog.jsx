cb19d9ce40646aac0a2c7c07735e61b46d795c9b
Marcem55 <malacalzamarcelo@gmail.com>
2024-08-20 23:14:12 -0300

    [ADD] exercise 5.7 completed

--- bloglist-frontend/src/components/Blog.jsx
+++ bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,40 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} - {blog.author}
-  </div>
-);
+import { useState } from "react";
+
+const containerStyles = {
+  display: "flex",
+  alignItems: "center",
+  gap: "12px",
+  border: "1px solid black",
+  padding: "2px",
+  margin: "6px",
+};
+
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState();
+
+  return (
+    <div
+      style={
+        showDetails
+          ? { border: "1px solid black", padding: "2px", margin: "6px" }
+          : containerStyles
+      }
+    >
+      <p>{blog.title}</p>
+      {showDetails && (
+        <div>
+          <p>{blog.url}</p>
+          <p>
+            Likes: {blog.likes} <button>like</button>
+          </p>
+          <p>{blog.author}</p>
+        </div>
+      )}
+      <button onClick={() => setShowDetails(!showDetails)}>
+        {showDetails ? "hide" : "view"}
+      </button>
+    </div>
+  );
+};
 
 export default Blog;
